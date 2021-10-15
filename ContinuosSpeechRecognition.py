3c9cae0f91748c9072dac9e58287568bf8cad4ee
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-10-15 19:29:22 +0200

    Update and rename SpeechRecognition.py to ContinuosSpeechRecognition.py

---
+++ ContinuosSpeechRecognition.py
@@ -0,0 +1,5 @@
+# import elements from libraries for Microsoft Speech Services and credential authentication 
+
+import azure.cognitiveservices.speech as speechsdk
+from azure.core.credentials import AzureKeyCredential
+
